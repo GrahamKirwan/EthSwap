2b7cd98d7acd99a2b9cc5b92727ff90db5c5ddcb
graham.kirwan4@gmail.com <graham.kirwan4@gmail.com>
2022-01-13 10:51:02 -0500

    update

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -10,7 +10,7 @@ module.exports = async function(deployer) {
   await deployer.deploy(EthSwap, token.address);
   const ethSwap = await EthSwap.deployed();
 
-  // Transfer all tokens to EthSwap (1 million)
+  // Transfer all tokens to EthSwap (1  million)
   await token.transfer(ethSwap.address, '1000000000000000000000000');
 
 };
