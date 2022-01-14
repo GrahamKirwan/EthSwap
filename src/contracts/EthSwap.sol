4e6cf4d82660e1af65f41e4c4a258e2a841e5068
graham.kirwan4@gmail.com <graham.kirwan4@gmail.com>
2022-01-13 21:56:00 -0500

    added event and require to ethswap contract

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -10,6 +10,13 @@ contract EthSwap {
     Token public token; // use our Token contract with 'token'
     uint public rate = 100; //uint cannot be negative or have decimels
 
+    event TokenPurchased(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+
     // Pass in the token address when contract is first ran, save as _token
     constructor(Token _token) public {
         token = _token;
@@ -19,8 +26,15 @@ contract EthSwap {
     function buyTokens() public payable {
         // Calculate the number of tokens to buy
         uint tokenAmount = msg.value * rate;
+
+        // Check that ethswap has enough tokens for this trade (require will act as a break if false)
+        require(token.balanceOf(address(this)) >= tokenAmount);
+
         // msg is the person calling the function, so here they will receive the tokens
         token.transfer(msg.sender, tokenAmount);
+
+        // Emit an event (trigger when something happens)
+        emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
     }
 }
 
